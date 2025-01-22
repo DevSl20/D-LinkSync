f13db7d1bd7ac7023f0d6449aeed9cfc1fd7a968
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2025-01-22 07:54:02 +0000

    save till now

---
+++ main.js
@@ -0,0 +1,6 @@
+require ('dotenv').config();
+
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(argv.input,output);
