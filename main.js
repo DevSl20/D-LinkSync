058aaca2b89c75ac06a433b6df3f0ff7afe738ca
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2025-01-28 08:01:21 +0000

    save till sir changes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-require ('dotenv').config();
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+
+// Handling CLI Arguments
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 const argv = yargs(hideBin(process.argv)).argv;
-console.log(argv.input,output);
+
+// Reading CLI Arguments
+const inputFile = argv.input;
+const outputFile = argv.clean;
+const reportFile = argv.report;
